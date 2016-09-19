76a9a17bbd867af4061fe0007171173c58978320
jeffrey leung <jleung@cs.utexas.edu>
2016-09-19 17:42:50 -0500

    init

---
+++ build/src/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/RayTracer.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/RayTracer.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/Box.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/Cone.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/Cylinder.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/Sphere.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/Square.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/SceneObjects/trimesh.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/fileio/bitmap.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/fileio/bitmap.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/fileio/buffer.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/fileio/buffer.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/fileio/pngimage.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/fileio/pngimage.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/main.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/main.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/parser/Parser.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/parser/Parser.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/parser/ParserException.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/parser/ParserException.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/parser/Token.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/parser/Token.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/parser/Tokenizer.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/camera.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/camera.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/cubeMap.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/cubeMap.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/light.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/light.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/material.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/material.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/ray.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/ray.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/scene/scene.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/scene/scene.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/CommandLineUI.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/CubeMapChooser.cxx" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/GraphicalUI.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/ModelerCamera.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/TraceGLWindow.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/debuggingView.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingView.cpp.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/debuggingWindow.cxx" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o"
+  "/u/jleung/Downloads/CS354_Graphics/A1/ray/src/ui/glObjects.cpp" "/u/jleung/Downloads/CS354_Graphics/A1/ray/build/src/CMakeFiles/ray.dir/ui/glObjects.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/lib/fltk"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/SceneObjects/trimesh.cpp
@@ -0,0 +1,163 @@
+#include <cmath>
+#include <float.h>
+#include <algorithm>
+#include <assert.h>
+#include <string.h>
+#include "trimesh.h"
+#include "../ui/TraceUI.h"
+extern TraceUI* traceUI;
+
+using namespace std;
+
+Trimesh::~Trimesh()
+{
+	for( Materials::iterator i = materials.begin(); i != materials.end(); ++i )
+		delete *i;
+}
+
+// must add vertices, normals, and materials IN ORDER
+void Trimesh::addVertex( const glm::dvec3 &v )
+{
+    vertices.push_back( v );
+}
+
+void Trimesh::addMaterial( Material *m )
+{
+    materials.push_back( m );
+}
+
+void Trimesh::addNormal( const glm::dvec3 &n )
+{
+    normals.push_back( n );
+}
+
+// Returns false if the vertices a,b,c don't all exist
+bool Trimesh::addFace( int a, int b, int c )
+{
+    int vcnt = vertices.size();
+
+    if( a >= vcnt || b >= vcnt || c >= vcnt ) return false;
+
+    TrimeshFace *newFace = new TrimeshFace( scene, new Material(*this->material), this, a, b, c );
+    newFace->setTransform(this->transform);
+    if (!newFace->degen) faces.push_back( newFace );
+
+
+    // Don't add faces to the scene's object list so we can cull by bounding box
+    // scene->add(newFace);
+    return true;
+}
+
+const char* Trimesh::doubleCheck()
+// Check to make sure that if we have per-vertex materials or normals
+// they are the right number.
+{
+    if( !materials.empty() && materials.size() != vertices.size() )
+        return "Bad Trimesh: Wrong number of materials.";
+    if( !normals.empty() && normals.size() != vertices.size() )
+        return "Bad Trimesh: Wrong number of normals.";
+
+    return 0;
+}
+
+bool Trimesh::intersectLocal(ray& r, isect& i) const
+{
+	typedef Faces::const_iterator iter;
+	bool have_one = false;
+	for( iter j = faces.begin(); j != faces.end(); ++j )
+	{
+		isect cur;
+		if( (*j)->intersectLocal( r, cur ) )
+		{
+			if( !have_one || (cur.t < i.t) )
+			{
+				i = cur;
+				have_one = true;
+			}
+		}
+	}
+	if( !have_one ) i.setT(1000.0);
+	return have_one;
+} 
+
+bool TrimeshFace::intersect(ray& r, isect& i) const {
+  return intersectLocal(r, i);
+}
+
+// Intersect ray r with the triangle abc.  If it hits returns true,
+// and put the parameter in t and the barycentric coordinates of the
+// intersection in u (alpha) and v (beta).
+bool TrimeshFace::intersectLocal(ray& r, isect& i) const
+{
+    // YOUR CODE HERE
+    glm::dvec3 a_coords = parent->vertices[ids[0]];
+    glm::dvec3 b_coords = parent->vertices[ids[1]];
+    glm::dvec3 c_coords = parent->vertices[ids[2]];
+
+    glm::dvec3 d_normalized = glm::normalize(r.d);
+//    glm::dvec3 n = glm::normalize(glm::cross(a_coords - c_coords, b_coords - c_coords));
+
+    if (glm::dot(normal, d_normalized) == 0 ) return false;
+    double d = glm::dot(normal, parent->vertices[0]);
+    double t = (d - glm::dot(normal, r.p)) / (glm::dot(normal, d_normalized));
+    glm::dvec3 q = r.p + t * d_normalized; 
+    
+
+    if (glm::dot(glm::cross(b_coords - a_coords, q - a_coords), normal) >= 0 
+        && glm::dot(glm::cross(c_coords - b_coords, q - b_coords), normal) >= 0
+        && glm::dot(glm::cross(a_coords - c_coords, q - c_coords), normal) >= 0) {
+        // compute barycentric coordinates
+        double alpha = glm::dot(glm::cross(c_coords - b_coords, q - b_coords), normal)
+            / glm::dot(glm::cross(b_coords - a_coords, c_coords - a_coords), normal);
+
+        double beta = glm::dot(glm::cross(a_coords - c_coords, q - c_coords), normal)
+            / glm::dot(glm::cross(b_coords - a_coords, c_coords - a_coords), normal);
+
+        double gamma = glm::dot(glm::cross(b_coords - a_coords, q - a_coords), normal)
+            / glm::dot(glm::cross(b_coords - a_coords, c_coords - a_coords), normal);
+
+        i.setUVCoordinates(glm::dvec2(alpha, beta));
+        i.setBary(q);
+        i.setT(t);
+        i.setN(normal);
+        i.setObject(this);
+        i.setMaterial(this->getMaterial());
+        return true;
+    } 
+    else {
+       return false;
+    } 
+    // FIXME: Add ray-trimesh intersection
+
+}
+
+void Trimesh::generateNormals()
+// Once you've loaded all the verts and faces, we can generate per
+// vertex normals by averaging the normals of the neighboring faces.
+{
+    int cnt = vertices.size();
+    normals.resize( cnt );
+    int *numFaces = new int[ cnt ]; // the number of faces assoc. with each vertex
+    memset( numFaces, 0, sizeof(int)*cnt );
+    
+    for( Faces::iterator fi = faces.begin(); fi != faces.end(); ++fi )
+    {
+		glm::dvec3 faceNormal = (**fi).getNormal();
+        
+        for( int i = 0; i < 3; ++i )
+        {
+            normals[(**fi)[i]] += faceNormal;
+            ++numFaces[(**fi)[i]];
+        }
+    }
+
+    for( int i = 0; i < cnt; ++i )
+    {
+        if( numFaces[i] )
+            normals[i]  /= numFaces[i];
+    }
+
+    delete [] numFaces;
+    vertNorms = true;
+}
+
